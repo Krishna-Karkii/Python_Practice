bdb9cbbe9ff59dc167e04e1627e5bf5e3bb41c7e
Krishna karki <121712782+krishna00o@users.noreply.github.com>
2024-04-07 14:49:36 +0545

    Add files via upload

---
+++ 1st Day/main.py
@@ -0,0 +1,6 @@
+User_prompt = "Enter a todo task : "
+Text1 = input(User_prompt)
+Text2 = input(User_prompt)
+Text3 = input(User_prompt)
+todos_list = [Text1, Text2, Text3]
+print(" i wanna", todos_list)
