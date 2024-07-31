797205a29625552c780c30fcf20fb765204802ea
krishna <krishnakarki1996@gmail.com>
2024-07-31 22:43:15 +0545

    create the basic window and run the main loop

--- stars/stars.py
+++ stars/stars.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from pygame.sprite import Sprite
 
@@ -12,3 +13,20 @@ class Stars(Sprite):
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.image = pygame.image.load("../images/star.png")
 
+    def run_stars(self):
+        """main loop of this function"""
+        while True:
+            for self.event in pygame.event.get():
+                if self.event.type == pygame.QUIT:
+                    sys.exit()
+
+                elif self.event.type == pygame.KEYDOWN:
+                    if self.event.key == pygame.K_q:
+                        sys.exit()
+
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    star = Stars()
+    star.run_stars()
