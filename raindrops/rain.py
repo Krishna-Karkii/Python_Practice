7451f51d39383e6738c1d6e4e0555b65219a04b9
krishna <krishnakarki1996@gmail.com>
2024-08-01 15:33:50 +0545

    initialize attributes of raindrop class

--- raindrops/rain.py
+++ raindrops/rain.py
@@ -5,7 +5,8 @@ from pygame.sprite import Sprite
 class Raindrop(Sprite):
     def __init__(self, rain_main):
         super().__init__()
-        pass
+        self.image = pygame.image.load("../images/rain_drop.png")
+        self.rect = self.image.get_rect()
 
 
 class Rain:
