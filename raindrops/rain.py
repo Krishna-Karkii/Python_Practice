a9f6e815789600893eb5969fd02f89d75cbb2af5
krishna <krishnakarki1996@gmail.com>
2024-08-01 15:29:16 +0545

    initialize needed resources

---
+++ raindrops/rain.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Raindrop(Sprite):
+    def __init__(self, rain_main):
+        super().__init__()
+        pass
+
+
+class Rain:
+    pass
