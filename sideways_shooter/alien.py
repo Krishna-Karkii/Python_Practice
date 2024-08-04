eb9c68276738593ab62aca92204325789c87f4e9
krishna <krishnakarki1996@gmail.com>
2024-08-04 22:14:49 +0545

    initialize the position of the alien ship

--- sideways_shooter/alien.py
+++ sideways_shooter/alien.py
@@ -1,3 +1,4 @@
+import pygame
 from pygame.sprite import Sprite
 
 
@@ -5,4 +6,9 @@ class Alien(Sprite):
     def __init__(self, sws_game):
         """initializes the necessary attributes"""
         super().__init__()
-        pass
+        self.screen_rect = sws_game.screen_rect
+
+        self.image = pygame.image.load("../images/alien.png")
+        self.image = pygame.transform.rotate(self.image, 90)
+        self.rect = self.image.get_rect()
+        self.rect.midright = self.screen_rect.midright
