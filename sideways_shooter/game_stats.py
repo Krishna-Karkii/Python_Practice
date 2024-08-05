7ce86d091f729f796ca286a34b2c538e44997c2d
krishna <krishnakarki1996@gmail.com>
2024-08-05 17:51:19 +0545

    initialize a new class to calculate game statistics

---
+++ sideways_shooter/game_stats.py
@@ -0,0 +1,8 @@
+class GameStats:
+    """This class covers everything related to game statistics.
+    ex: game score, number of ships left"""
+    def __init__(self):
+        """Initialize the default statistics"""
+        super().__init__()
+        pass
+
