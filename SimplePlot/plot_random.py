e1f3f45ba0df31a5fd7b3cc966b8c76b0f4f4bfd
krishna <krishnakarki1996@gmail.com>
2024-08-23 22:14:33 +0545

    add loop to keep generating

--- SimplePlot/plot_random.py
+++ SimplePlot/plot_random.py
@@ -1,11 +1,20 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.generate_walk()
 
-fig, ax = plt.subplots()
+while True:
+    # create the random walk generate
+    rw = RandomWalk()
+    rw.generate_walk()
 
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    # fig for the figure, ax for plot
+    fig, ax = plt.subplots()
 
-plt.show()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect("equal")
+    plt.show()
+
+    # ask if to generate the graph again
+    keep_generation = input("Do you want to generate walk? (y/n): ")
+    if keep_generation.strip().capitalize() == "N":
+        break
